83d96bb54b3e7a85ae0fc3b3243bafe010fe379d
antonius <a.hristoff@gmail.com>
2025-06-18 01:22:09 +0100

    Create RolesLib.sol
    Signed-off-by: antonius <a.hristoff@gmail.com>

---
+++ RolesLib.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+library RolesLib {
+    string constant ANCHOR = "Anchor";
+    string constant ARCHITECT = "Architect";
+    string constant STEWARD = "Steward";
+    string constant INITIATOR = "Initiator";
+}
