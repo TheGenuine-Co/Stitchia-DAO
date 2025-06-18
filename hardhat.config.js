1a3b20a30d0dfbb89b6d996a414c1a3ddba4ef32
antonius <a.hristoff@gmail.com>
2025-06-18 01:27:51 +0100

    Update hardhat.config.js
    Signed-off-by: antonius <a.hristoff@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-verify");
-require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.21",
   networks: {
-    holesky: {
-      url: `https://holesky.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+    hardhat: {},
   },
-  etherscan: {
-    apiKey: {
-      holesky: process.env.HOLESKY_ETHERSCAN_API_KEY
-    }
-  }
 };
