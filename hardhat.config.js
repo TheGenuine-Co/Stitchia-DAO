23cdcd7be907e3fbe18345b17f26039f077ebe04
antonius <a.hristoff@gmail.com>
2025-07-07 10:47:28 +0000

    Pending changes exported from your codespace

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    holesky: {
+      url: `https://holesky.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      holesky: process.env.HOLESKY_ETHERSCAN_API_KEY
+    }
+  }
 };
